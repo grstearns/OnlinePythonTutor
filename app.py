4d7692cef8f6d2d08530784349a5950f23d3ae23
Greg Stearns <greg@gregthetech.com>
2012-06-12 17:17:20 -0400

    LEMON CURRY

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from flask import Flask
+from flask import request
+
+
 app = Flask(__name__)
+
+# url_for('static')
+
+
+@app.route('/execute', methods=['POST'])
+def execute():
+	# user_script = request.form.post['user_script']
+	# return request
+
+	return 'LEMON CURRY'
+
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+
 
 if __name__ == '__main__':
     app.run()
