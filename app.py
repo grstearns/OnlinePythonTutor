771f16b3b66503c9da338279e6d02414378bf810
Jason Laster <Jason.laster.11@gmail.com>
2012-06-15 18:31:14 -0400

    setup heroku req.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import request
 from flask import render_template
@@ -22,5 +23,6 @@ def root():
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
+  # app.debug = True
